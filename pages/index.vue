71dbfb844469da655d544df52efe70cb10c5b498
Victor <victor@gbtsbuildingmaterials.com>
2025-06-13 23:36:45 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,5 @@
-<script setup>
+ <script setup>
+import { ref } from 'vue'
 import pageData from '@/data/index.json'
 const page = ref(pageData)
 
@@ -15,22 +16,47 @@ definePageMeta({
       :image="page.hero.image"
       :imageAlt="page.hero.imageAlt"
       :buttons="page.hero.buttons"
-    ></Hero>
-    <Logos :title="page.logos.title" :icons="page.logos.icons"></Logos>
+    />
     <Features
       :title="page.features.title"
       :description="page.features.description"
       :items="page.features.items"
-    ></Features>
-    <Testimonials
-      :title="page.testimonials.title"
-      :description="page.testimonials.description"
-      :items="page.testimonials.items"
-    ></Testimonials>
-    <Cta
-      :title="page.cta.title"
-      :description="page.cta.description"
-      :buttons="page.cta.buttons"
-    ></Cta>
+    />
+
+    <!-- Main Sections -->
+    <div>
+      <SectionCard
+        v-for="section in page.sections"
+        :key="section.title"
+        :title="section.title"
+        :subtitle="section.subtitle"
+        :description="section.description"
+        :image="section.image"
+        :cta="section.cta"
+      />
+    </div>
+
+    <!-- Blog Excerpts -->
+    <div>
+      <BlogExcerpt
+        v-for="blog in page.blogExcerpts"
+        :key="blog.slug"
+        :title="blog.title"
+        :excerpt="blog.excerpt"
+        :author="blog.author"
+        :date="blog.date"
+        :image="blog.image"
+        :readMoreUrl="blog.readMoreUrl"
+      />
+    </div>
+
+    <!-- Footer -->
+    <FooterComponent
+      :links="page.footer.links"
+      :address="page.footer.address"
+      :contact="page.footer.contact"
+      :social="page.footer.social"
+      :copyright="page.footer.copyright"
+    />
   </Container>
 </template>
