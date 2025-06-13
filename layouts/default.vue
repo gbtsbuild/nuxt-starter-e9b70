cfadea8b927c29024c80ad8bdc89880f7520356c
Victor <victor@gbtsbuildingmaterials.com>
2025-06-14 00:04:12 +0100

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,19 @@
 <template>
-  <Navbar></Navbar>
-  <slot></slot>
-  <Footer></Footer>
+  <Navbar />
+  <slot />
+  <Footer
+    :links="page.footer.links"
+    :address="page.footer.address"
+    :contact="page.footer.contact"
+    :social="page.footer.social"
+    :copyright="page.footer.copyright"
+  />
 </template>
+
+<script setup>
+import { ref } from 'vue'
+import pageData from '@/data/index.json'
+
+// Provide page data to Footer (and other components if needed)
+const page = ref(pageData)
+</script>
