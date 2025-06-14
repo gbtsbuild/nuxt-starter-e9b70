2d00eb1016c97a16157cfacec6d0e755dc1b371a
Victor <victor@gbtsbuildingmaterials.com>
2025-06-14 08:46:33 +0100

    Update nuxt.config.ts
    Removed import clause

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import tailwindcss from "@tailwindcss/vite";
-
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
