031fe4f68d7e17ac5abcd3ecfc61db8096a82679
Victor <victor@gbtsbuildingmaterials.com>
2025-06-14 10:24:24 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,5 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  css: ["~/assets/css/main.css"],
-  vite: {
-    plugins: [
-      tailwindcss(),
-    ],
-  },
-  experimental: { appManifest: false },
-  modules: ["nuxt-icon", "@nuxt/image"],
-  compatibilityDate: "2024-12-18",
-});
+  modules: [
+    '@nuxtjs/tailwindcss',
+  ],
+})
